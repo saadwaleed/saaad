e6818a7275c4c2687f5a96873d42084f820e7d30
saadwaleed <45393060+saadwaleed@users.noreply.github.com>
2019-03-31 23:05:54 +0300

    Add files via upload

---
+++ سعد الصقر.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+
+
+using namespace std;
+int main()
+{
+	int x, y, sum;
+	cout << "rnter 2 numbeer" << endl;
+		cin >> x >> y;
+		sum = x + y;
+		cout << sum;
+
+
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
